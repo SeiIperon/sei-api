e354aba9f672dc5dca076d92619f96151e328f0e
alex.vieira <alex.vieira@memora.com.br>
2021-08-27 17:14:16 -0300

    Inclusão dos endpoints consultarSeries e incluirDocumento

--- src/main/java/br/gov/ro/pge/sei/api/controller/SeiController.java
+++ src/main/java/br/gov/ro/pge/sei/api/controller/SeiController.java
@@ -4,9 +4,13 @@
 package br.gov.ro.pge.sei.api.controller;
 
 import br.gov.ro.pge.sei.api.domain.fault.SeiFaultException;
+import br.gov.ro.pge.sei.api.domain.parameter.ParametrosConsultarSeriesWS;
 import br.gov.ro.pge.sei.api.domain.parameter.ParametrosGerarProcedimentoWS;
+import br.gov.ro.pge.sei.api.domain.parameter.ParametrosIncluirDocumentoWS;
 import br.gov.ro.pge.sei.api.domain.parameter.ParametrosListarExtensoesPermitidasWS;
+import br.gov.ro.pge.sei.api.domain.response.RespostaConsultarSeriesWS;
 import br.gov.ro.pge.sei.api.domain.response.RespostaGerarProcedimentoWS;
+import br.gov.ro.pge.sei.api.domain.response.RespostaIncluirDocumentoWS;
 import br.gov.ro.pge.sei.api.domain.response.RespostaListarExtensoesPermitidasWS;
 import br.gov.ro.pge.sei.api.service.SeiService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,4 +39,17 @@ public class SeiController {
         RespostaGerarProcedimentoWS out = this.seiService.gerarProcedimento(param);
         return ResponseEntity.ok(out);
     }
+
+    @PostMapping(value = "/incluir-documento", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<RespostaIncluirDocumentoWS> incluirDocumento(@RequestBody(required = false) ParametrosIncluirDocumentoWS param) throws SeiFaultException {
+        RespostaIncluirDocumentoWS out = this.seiService.incluirDocumento(param);
+        return ResponseEntity.ok(out);
+    }
+
+    @GetMapping(value = "/consultar-series", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<RespostaConsultarSeriesWS> consultarSeries(@RequestBody(required = false) ParametrosConsultarSeriesWS param) throws SeiFaultException {
+        RespostaConsultarSeriesWS out = this.seiService.consultarSeries(param);
+        return ResponseEntity.ok(out);
+    }
+
 }
